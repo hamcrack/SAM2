a30848a4cd447016f252f54721dc0aa702120b58
hamcrack <hamishcrackett@gmail.com>
2025-04-23 17:19:17 +0200

    some final changes needed

--- my_test.py
+++ my_test.py
@@ -55,8 +55,6 @@ model_cfg = "configs/sam2.1/sam2.1_hiera_b+.yaml"
 sam2 = build_sam2(model_cfg, sam2_checkpoint, device=device, apply_postprocessing=False)
 
 urls_string = "https://images.coches.com/_vn_/kia/Sportage/c399cf1d98a95d24f8e8715dd0b13fb2.jpg?p=cc_vn_high https://www.hyundai.com/es/zonaeco/image/catalog/articulos/ecodrive/electricos-2021/hyundai-nuevo-KONA-electrico-01.jpg https://www.topgear.com/sites/default/files/news-listicle/image/2022/11/p90419422_lowres_the-new-mini-3-door-_0.jpg?w=827&h=465 https://www.topgear.com/sites/default/files/2023/01/51952307109_b9b8d85293_k.jpeg?w=827&h=465 https://www.topgear.com/sites/default/files/2022/02/hyundai-uk-all-new-tucson-1220-50.jpeg?w=827&h=465 https://media.evo.co.uk/image/private/s--I_T1JLxX--/f_auto,t_content-image-full-desktop@1/v1696526185/evo/2023/10/BMW%20M2%20v%20Porsche%20Cayman%20GT4%20v%20Alpine%20A110%20R%20on%20road-11.jpg https://stage-drupal.car.co.uk/s3fs-public/styles/original_size/public/2019-09/why-are-number-plates-yellow-and-white.jpg?rt1UJUyIi7L2DpS613hFYlI5ng3U4QT3&itok=3SZjXU0B https://images.pistonheads.com/nimg/47298/blobid0.jpg https://mercedes-benz-club.co.uk/wp-content/uploads/elementor/thumbs/banner-image-1280x720-02-q1a5qhcmx0l65yryp4if6yzr8qhn55kpfwrneobd68.jpg https://static.dw.com/image/44096983_1004.webp https://media.euobserver.com/69f73b65af9807ac139dbd84a4473052.jpg"
-# urls_string = "https://www.topgear.com/sites/default/files/2023/01/51952307109_b9b8d85293_k.jpeg?w=827&h=465 https://www.topgear.com/sites/default/files/2022/02/hyundai-uk-all-new-tucson-1220-50.jpeg?w=827&h=465 https://media.evo.co.uk/image/private/s--I_T1JLxX--/f_auto,t_content-image-full-desktop@1/v1696526185/evo/2023/10/BMW%20M2%20v%20Porsche%20Cayman%20GT4%20v%20Alpine%20A110%20R%20on%20road-11.jpg https://stage-drupal.car.co.uk/s3fs-public/styles/original_size/public/2019-09/why-are-number-plates-yellow-and-white.jpg?rt1UJUyIi7L2DpS613hFYlI5ng3U4QT3&itok=3SZjXU0B https://images.pistonheads.com/nimg/47298/blobid0.jpg https://mercedes-benz-club.co.uk/wp-content/uploads/elementor/thumbs/banner-image-1280x720-02-q1a5qhcmx0l65yryp4if6yzr8qhn55kpfwrneobd68.jpg https://static.dw.com/image/44096983_1004.webp https://media.euobserver.com/69f73b65af9807ac139dbd84a4473052.jpg"
-# urls_string = "https://static.dw.com/image/44096983_1004.webp https://images.coches.com/_vn_/kia/Sportage/c399cf1d98a95d24f8e8715dd0b13fb2.jpg?p=cc_vn_high"
 image_urls = urls_string.split()
 
 debug = True
@@ -82,12 +80,8 @@ with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
         image = img_from_url(img_url)
         display = copy.deepcopy(image)
         masks = mask_generator.generate(image)
-        print(len(masks), " masks with keys", masks[0].keys())
-        # plt.figure(figsize=(20, 20))
-        # plt.imshow(image)
-        # show_anns(masks)
-        # plt.axis('off')
-        # plt.show()
+        if debug:
+            print(len(masks), " masks with keys", masks[0].keys())
         for i in range(len(masks)):
             f_x, f_y, f_w, f_h = masks[i]['bbox']
             x, y, w, h = int(f_x), int(f_y), int(f_w), int(f_h)
@@ -95,30 +89,33 @@ with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
             box_area = w * h
             ratio = w / h
             occupied = seg_area / box_area
-            print("Seg ", i, " seg area: ", seg_area, " / box area: ", box_area," = occupied: ", occupied)
-            print("    Box x, y, w, h: ", x, y, w, h, " ratio: ", ratio)
+            if debug:
+                print("Seg ", i, " seg area: ", seg_area, " / box area: ", box_area," = occupied: ", occupied)
+                print("    Box x, y, w, h: ", x, y, w, h, " ratio: ", ratio)
             color = (0, 0, 255)
             if min_plate_area < seg_area < max_plate_area:
-                print("    Found potential plate: ", min_plate_area, "<",  seg_area, "<", max_plate_area)
+                if debug:
+                    print("    Found potential plate: ", min_plate_area, "<",  seg_area, "<", max_plate_area)
                 if min_w_h_ratio < ratio < max_w_h_ratio and occupied > min_occupied_percentage:
                     color = (255, 0, 0)
                     potential_plate = copy.deepcopy(image[y:y+h, x:x+w])
+                    display_plate = copy.deepcopy(potential_plate)
                 
-                    # potential_plate_gray = cv2.cvtColor(potential_plate, cv2.COLOR_BGR2GRAY)
-                    # ret2, potential_plate_bin = cv2.threshold(potential_plate_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-                    # # potential_plate = cv2.resize(potential_plate, (int(w * scale), int(h * scale)))
+                    potential_plate_gray = cv2.cvtColor(potential_plate, cv2.COLOR_BGR2GRAY)
+                    ret2, potential_plate_bin = cv2.threshold(potential_plate_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+                    # TODO get a range for the bounding box
+                    # TODO and put center check back in but maybe not needed with above
                 
                     sub_masks = mask_generator.generate(potential_plate)
                     no_sub_masks = len(sub_masks)
                     s_w, s_h = len(potential_plate[0]), len(potential_plate)
                     x_bound = int(s_w / 6)
                     y_bound = int(s_h / 6)
-                    print("    ", no_sub_masks, " sub masks - bounds: ", x_bound, y_bound, " seg area: ", seg_area)
-                    display_plate = cv2.rectangle(potential_plate, (x_bound, y_bound), (len(potential_plate[0])-x_bound, len(potential_plate)-y_bound), (255, 0, 0), 2)
+                    if debug:
+                        print("    ", no_sub_masks, " sub masks - bounds: ", x_bound, y_bound, " seg area: ", seg_area)
+                    if show_overlays:
+                        display_plate = cv2.rectangle(display_plate, (x_bound, y_bound), (len(potential_plate[0])-x_bound, len(potential_plate)-y_bound), (255, 0, 0), 2)
                     no_valid_sub_masks = 0
-                    ave_valid_x = 0
-                    ave_valid_y = 0
                     centers_x = np.array([])
                     centers_y = np.array([])
                     std_err = 1
@@ -127,45 +124,46 @@ with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
                         sub_seg_area = sub_masks[j]['area']
                         sub_center_x, sub_center_y = int(sf_x + sf_w/2), int(sf_y + sf_h/2)
                         sub_color = (0, 0, 255)
-                        print("        Sub mask ", j, " centers: ", sub_center_x, sub_center_y, "sub seg area: ", sub_seg_area, ", ratio: ", sub_seg_area/seg_area)
+                        if debug:
+                            print("        Sub mask ", j, " centers: ", sub_center_x, sub_center_y, "sub seg area: ", sub_seg_area, ", ratio: ", sub_seg_area/seg_area)
 
-                        avg_color_per_row = np.average(potential_plate, axis=0)
-                        avg_color = np.average(avg_color_per_row, axis=0)
-                        hsv_img = cv2.cvtColor(np.array([[avg_color, avg_color], [avg_color, avg_color]], dtype=np.uint8), cv2.COLOR_BGR2HSV)
-                        its_blue = False
-                        print("         ave color: ", avg_color, ", hue:", hsv_img[0][0])
-                        if 100 < int(hsv_img[0][0][0]) < 110 and 126 < int(hsv_img[0][0][1]):
-                            print("         its blue")
+                        avg_color = potential_plate[sub_center_y][sub_center_x]
+                        hsv_img = cv2.cvtColor(np.array([[avg_color, avg_color], [avg_color, avg_color]], dtype=np.uint8), cv2.COLOR_RGB2HSV)
+                        # TODO something is wrong with the color conversion
+                        if debug:
+                            print("         ave color: ", avg_color, ", hsv:", hsv_img[0][0])
+                            if 115 < int(hsv_img[0][0][0]) < 130 and 126 < int(hsv_img[0][0][1]) and int(hsv_img[0][0][2]) > 50:
+                                print("         its blue")
+                            if avg_color[0] > 110 and avg_color[1] < 150 and avg_color[2] < 60:
+                                print("         its blue 2")
 
                         if x_bound < sub_center_x < s_w  - x_bound and \
                         y_bound < sub_center_y < s_h - y_bound and min_sub_ratio < sub_seg_area/seg_area < max_sub_ratio:
                             no_valid_sub_masks += 1
                             centers_x = np.append(centers_x, sub_center_x)
                             centers_y = np.append(centers_y, sub_center_y)
-                            ave_valid_x += sub_center_x
-                            ave_valid_y += sub_center_y
                             sub_color = (0, 255, 0)
-                        display_plate = cv2.circle(display_plate, (sub_center_x, sub_center_y), 3, sub_color, 2) 
-                        display_plate = cv2.rectangle(display_plate, (int(sf_x), int(sf_y)), (int(sf_x + sf_w), int(sf_y + sf_h)), (255, 0, 0), 2)
+                        if show_overlays:
+                            display_plate = cv2.circle(display_plate, (sub_center_x, sub_center_y), 3, sub_color, 2) 
+                            display_plate = cv2.rectangle(display_plate, (int(sf_x), int(sf_y)), (int(sf_x + sf_w), int(sf_y + sf_h)), (255, 0, 0), 2)
+                   
                     if no_valid_sub_masks > 3:
-                        ave_valid_x = int(ave_valid_x / no_valid_sub_masks)
-                        ave_valid_y = int(ave_valid_y / no_valid_sub_masks)
-
                         slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(centers_x, centers_y)
-                        print(f"    plate slope: {slope}, intercept: {intercept}, r_value: {r_value}, p_value: {p_value}, std_err: {std_err}")
-                    ave_cent_dist = math.sqrt((s_w/2 - ave_valid_x)**2 + (s_h/2 - ave_valid_y)**2)
-                    print("    Average center distance: ", ave_cent_dist, "no_valid_sub_masks: ", no_valid_sub_masks)
-                    display_plate = cv2.circle(display_plate, (ave_valid_x, ave_valid_y), 3, (255, 0, 0), 2) 
-                    if no_valid_sub_masks > 3 and ave_cent_dist < y_bound*2 and std_err < max_std_err:
-                        print("    Found ", no_valid_sub_masks, " valid sub masks")
-                        color = (0, 255, 0)
-                        cv2.imshow('potential_plate', display_plate)
+                        if debug:
+                            print(f"    plate slope: {slope}, intercept: {intercept}, r_value: {r_value}, p_value: {p_value}, std_err: {std_err}")
+                            if std_err < max_std_err:
+                            if debug:
+                                print("    Found ", no_valid_sub_masks, " valid sub masks")
+                            color = (0, 255, 0)
+                    if show_images:
+                        cv2.imshow('display_plate', display_plate)
                         cv2.waitKey(0)
                         cv2.destroyAllWindows()
+            if show_overlays:
+                display = cv2.rectangle(display, (x, y), (x + w, y + h), color, 2)
+                display = cv2.putText(display, str(i), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
 
-            display = cv2.rectangle(display, (x, y), (x + w, y + h), color, 2)
-            display = cv2.putText(display, str(i), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
-                                
-        cv2.imshow('THEKER image', display)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        if show_images:                    
+            cv2.imshow('THEKER image', display)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
